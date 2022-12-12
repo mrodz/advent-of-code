3b7e1be70ac89d0433092e98d3d5edb39554cd85
mrodz <79176075+mrodz@users.noreply.github.com>
2022-12-11 16:51:11 -0800

    dec 3rd (part 2)

--- dec3rd/solution.rs
+++ dec3rd/solution.rs
@@ -1,26 +1,36 @@
 use std::path::Path;
 use std::io::{self, Lines, BufReader, BufRead};
 use std::fs::File;
-use std::collections::HashSet;
+use std::collections::{BTreeMap};
 
 pub fn main() {
-	let lines = read_lines("./input.txt").expect("no input");
+	let mut lines = read_lines("./input.txt").expect("no input");
 
 	let mut sum: u32 = 0;
 
-	for line in lines {
-		let line = line.unwrap();
-
-		let line: (&str, &str) = line.split_at(line.len() / 2);
-
-		for occurrence in common_occurrences(line.0, line.1) {
-			let weight = weight_of_occurrence(occurrence);
-			dbg!(weight);
-			sum += weight;
-		}
+	while let (
+		Some(Ok(sack1)), 
+		Some(Ok(sack2)), 
+		Some(Ok(sack3))
+	) = (lines.next(), lines.next(), lines.next()) {
+		sum += weight_of_occurrence(get_group_badge(&sack1, &sack2, &sack3));
 	}
 
 	dbg!(sum);
+
+	// for line in lines {
+	// 	let line = line.unwrap();
+
+	// 	let line: (&str, &str) = line.split_at(line.len() / 2);
+
+	// 	for occurrence in get_group_badge(line.0, line.1) {
+	// 		let weight = weight_of_occurrence(occurrence);
+	// 		dbg!(weight);
+	// 		sum += weight;
+	// 	}
+	// }
+
+	// dbg!(sum);
 }
 
 pub fn weight_of_occurrence(c: char) -> u32 {
@@ -32,28 +42,51 @@ pub fn weight_of_occurrence(c: char) -> u32 {
 	}
 } 
 
-pub fn common_occurrences<'a>(bottom: &'a str, top: &'a str) -> HashSet<char> {
-	let size = bottom.len();
+pub fn get_group_badge<'a>(first: &String, second: &String, third: &String) -> char {
+	use std::cmp::max;
+	let max_len: usize = max(first.len(), max(second.len(), third.len()));
 
-	let mut bottom_characters: HashSet<char> = HashSet::with_capacity(size);
-	let mut top_characters: HashSet<char> = HashSet::with_capacity(size);
-	let mut result: HashSet<char> = HashSet::with_capacity(size * 2);
+	let mut occurrences: BTreeMap<char, u8> = BTreeMap::new();
 
-	for chars in bottom.chars().zip(top.chars()) {
-		if bottom_characters.contains(&chars.1) {
-			result.insert(chars.1);
-		} else {
-			top_characters.insert(chars.1);
-		}
+	let first = first.as_bytes();
+	let second = second.as_bytes();
+	let third = third.as_bytes();
 
-		if top_characters.contains(&chars.0) {
-			result.insert(chars.0);
-		} else {
-			bottom_characters.insert(chars.0);
+
+	for i in 0..max_len {
+		if let Some(c) = first.get(i) {
+			let c = *c as char;
+			let count = *occurrences.get(&c).unwrap_or(&0) | 0b001;
+
+			if count == 0b111 {
+				return c;
+			}
+
+			occurrences.insert(c, count);
+		} 
+		if let Some(c) = second.get(i) {
+			let c = *c as char;
+			let count = *occurrences.get(&c).unwrap_or(&0) | 0b010;
+
+			if count == 0b111 {
+				return c;
+			}
+
+			occurrences.insert(c, count);
+		} 
+		if let Some(c) = third.get(i) {
+			let c = *c as char;
+			let count = *occurrences.get(&c).unwrap_or(&0) | 0b100;
+
+			if count == 0b111 {
+				return c;
+			}
+
+			occurrences.insert(c, count);
 		}
 	}
 
-	result
+	unreachable!()
 }
 
 fn read_lines<T: AsRef<Path>>(filename: T) -> io::Result<Lines<BufReader<File>>> {
