bdcd8ae06f26c2d7e0759aec62b5ee09c2953c78
Mateo Rodriguez <79176075+mrodz@users.noreply.github.com>
2022-12-14 19:45:23 +0000

    add to replit

--- .ccls-cache/@home@runner@advent-of-code/dec4th@solution.c
+++ .ccls-cache/@home@runner@advent-of-code/dec4th@solution.c
@@ -18,7 +18,7 @@ range_t *from_str(char *str) {
 
 	int non_int_at[3];
 
-	for (int i = 0, c = 0; i < len && c < 3; i++) {
+	for (int i = 0, c = 0; i < len; i++) {
 		if (!isdigit(str[i])) {
 			non_int_at[c++] = i;
 		}
@@ -52,7 +52,7 @@ int send_ranges_to_file(FILE *fp, void(*part)(range_t*, range_t*, void*), void *
 	return 0;
 }
 
-void fully_contained(range_t *r1, range_t *r2, void *data) {
+void duplicate_ranges(range_t *r1, range_t *r2, void *data) {
 	int *data_p = (int*) data;
 	if (r1->end >= r2->end && r1->start <= r2->start) {
 		*data_p += 1;
@@ -61,41 +61,13 @@ void fully_contained(range_t *r1, range_t *r2, void *data) {
 	}
 }
 
-int fits_range(range_t *r1, int num, int i) {
-  int result = num >= r1->start && num <= r1->end;
-  // if (result) {
-  // print_range(r1);
-  // printf("\t[%d] %d\n", i, num);  
-  // }
-  return result;
-}
-
-void partially_contained(range_t *r1, range_t *r2, void *data) {
-  int *data_p = (int*) data;
-  if (!((fits_range(r1, r2->start,1) || fits_range(r1, r2->end,2)) &&
-    (fits_range(r2, r1->start,3) || fits_range(r2, r2->end,4)))) {
-    printf("contained:\n");
-    print_range(r1);
-    print_range(r2);
-
-    *data_p += 1;
-    
-    } 
-  
-}
-
 int main(void) {
-	FILE *fp = fopen("./input2.txt", "r");
+	FILE *fp = fopen("./input.txt", "r");
 	if (fp == NULL) return 1;
 
 	int result = 0;
 
-	send_ranges_to_file(fp, &fully_contained, &result);
+	send_ranges_to_file(fp, &duplicate_ranges, &result);
+
 	printf("PART ONE:\n\t%d\n", result);
-
-  result = 0;
-  rewind(fp);
-
-  send_ranges_to_file(fp, &partially_contained, &result);
-	printf("PART TWO:\n\t%d\n", result);
 }
